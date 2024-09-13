0766d88b8e1420be561e07835c587cd51e2be642
ASCENDERS111 <bijoyk@raptorsupplies.com>
2024-09-13 21:32:25 +0530

    pol

---
+++ eccn_all.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+# Run ff.py
+print("Running eccn.py...")
+subprocess.run(["python", "eccn_2_Grainger.py"])
+
+# Wait for 5 seconds (optional, if you need to delay between scripts)
+time.sleep(1)
+
+# Run gg.py
+print("Running eccn.py...")
+subprocess.run(["python", "eccn.py"])
+time.sleep(1)
+
+# Run gg.py
+print("Running eccn.py...")
+subprocess.run(["python", "eccn_MCM_polo.py"])
+
+print("Both scripts have finished running.")

c2a0ba3dd1c6f6902b1e9ed981bd83bbd7cdf901
ASCENDERS111 <bijoyk@raptorsupplies.com>
2024-09-26 10:13:29 +0530

    MCM

--- eccn_MCM.py
+++ eccn_MCM.py
@@ -57,7 +57,7 @@ def fetch_data_from_zoho():
         df[['Remarks','CRM Update Status']]=''
 
         # Reorder columns as needed
-        ordered_columns = ['Subform_id', 'Lead Zoho ID', 'version_id', 'Product_id', 'Product Name', 'Version Sheet.Stage', 'Raptor Invoice','grkey', 'Date of Order Received', 'Shipping Country', 'Inco Term', 'Raptor SKU',
+        ordered_columns = ['Subform_id', 'Lead Zoho ID', 'version_id', 'Product_id','SNO', 'Product Name', 'Version Sheet.Stage', 'Raptor Invoice','grkey', 'Date of Order Received', 'Shipping Country', 'Inco Term', 'Raptor SKU',
                            'Grainger SKU', 'Grainger/Non_Grainger', 'Rpt_Billing_Entity_supplier', 'ECCN', 'HS_code', 'COO''Remarks','CRM Update Status'
                            ]
         df = df.reindex(columns=ordered_columns, fill_value=None)
@@ -140,7 +140,7 @@ def sort_and_append_to_gsheets(zoho_df, gsheets_df, sheet_name, worksheet_name, 
     sorted_df = sorted_df.replace({'NaT': '', 'NaN': '', np.nan: ''})
 
     # Reorder columns as needed
-    new_order = ['Subform_id', 'Lead Zoho ID', 'version_id', 'Product_id', 'Product Name', 'Version Sheet.Stage', 'Raptor Invoice','grkey', 'Date of Order Received', 'Shipping Country', 'Inco Term', 'Raptor SKU',
+    new_order = ['Subform_id', 'Lead Zoho ID', 'version_id', 'Product_id', 'SNO','Product Name', 'Version Sheet.Stage', 'Raptor Invoice','grkey', 'Date of Order Received', 'Shipping Country', 'Inco Term', 'Raptor SKU',
                  'Grainger SKU', 'Grainger/Non_Grainger', 'Rpt_Billing_Entity_supplier', 'ECCN', 'HS_code', 'COO','Remarks','CRM Update Status']
     sorted_df = sorted_df[new_order]
 
